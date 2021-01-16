49e179e64ea144d9aae1e25763c33da6d34a073d
Gary Kerr <garykerrwork@gmail.com>
2021-01-16 14:34:31 +0000

    Initial Commit

---
+++ NibbleRNG/NibbleRNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  NibbleRNGApp.swift
+//  NibbleRNG
+//
+//  Created by gary on 16/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct NibbleRNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
