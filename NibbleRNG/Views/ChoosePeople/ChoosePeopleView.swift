7081975fc1c293132cb60cfa1351d1c133b56593
Gary Kerr <garykerrwork@gmail.com>
2021-03-21 17:50:05 +0000

    Add done button to people selector

--- NibbleRNG/Views/ChoosePeople/ChoosePeopleView.swift
+++ NibbleRNG/Views/ChoosePeople/ChoosePeopleView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ChoosePeopleView: View {
+    @Environment(\.presentationMode) var presentationMode
     @ObservedObject var model = ChoosePeopleVM()
 
 
@@ -17,26 +18,47 @@ struct ChoosePeopleView: View {
 
 
     var body: some View {
-        List {
-            ForEach(People.people) { person in
-                Button(action: { model.select(person) }) {
-                    HStack {
-                        person.image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 80)
-                        Text(person.name)
-                            .font(.system(.title, design: .rounded))
-                        Spacer()
-                        if model.isSelected(person) {
-                            Image(systemName: "checkmark")
-                                .foregroundColor(.orange)
-                                .font(.system(size: 25, weight: .bold, design: .rounded))
+        NavigationView {
+            List {
+                ForEach(People.people) { person in
+                    Button(action: { model.select(person) }) {
+                        HStack {
+                            person.image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 80)
+                            Text(person.name)
+                                .font(.system(.title, design: .rounded))
+                            Spacer()
+                            if model.isSelected(person) {
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(.orange)
+                                    .font(.system(size: 25, weight: .bold, design: .rounded))
+                            }
                         }
                     }
                 }
             }
+            .navigationBarTitle(Text("People"), displayMode: .inline)
+            .navigationBarItems(trailing: doneButton)
         }
+    }
+}
+
+
+// MARK: - Private
+
+private extension ChoosePeopleView {
+    private var doneButton: some View {
+        Button(action: dismiss) {
+            Text("Done")
+                .foregroundColor(.orange)
+        }
+    }
+
+
+    private func dismiss() {
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
